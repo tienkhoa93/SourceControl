78c2a40751f3263475c0eafaadf51fcb1c330f70
Thomson1608 <nguoiay1608@gmail.com>
2017-04-06 13:14:09 +0700

    v1

--- QLNT/Program.cs
+++ QLNT/Program.cs
@@ -15,6 +15,7 @@ namespace QLNT
         static void Main()
         {
             Application.EnableVisualStyles();
+            //
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
         }
