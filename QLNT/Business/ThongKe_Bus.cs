bf6fc178de0f7b099e36c3b4e87de1f2c4ac9a84
DESKTOP-3CVFFK3\Hung <hunght2407@gmail.com>
2017-05-13 20:25:31 +0700

    add new features

---
+++ QLNT/Business/ThongKe_Bus.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using QLNT.LinQToSQL;
+namespace QLNT.Business
+{
+  public  class ThongKe_Bus
+  {
+      QLNTDataContext dtcontent = new QLNTDataContext();
+      public class ThongKeKhu
+      {
+           public String TenKhu { get; set; }
+           public int SoNguoi { get; set; }
+      }
+
+      public class ThongKeNamNu_
+      {
+         public String GioiTinh { get; set; }
+         public int TongSo { get; set; }
+      }
+       
+    }
+}
