1830fbf343e47fb68e28a0bdfed4e2ead5d15890
DESKTOP-1MINTP7\Desktop <minhtan1608@gmail.com>
2017-05-14 09:05:31 +0700

    Create file NguoiDung

---
+++ QLNT/Business/SysNguoiDung_Bus.cs
@@ -0,0 +1,106 @@
+﻿using QLNT.LinQToSQL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QLNT.Business
+{
+    public class SysNguoiDung_Bus
+    {
+        QLNTDataContext dtcontent = new QLNTDataContext();
+        public List<SysUser> ListNguoiDung()
+        {
+            var c = from dt in dtcontent.SysUsers
+                    select dt;
+            return c.ToList<SysUser>();
+        }
+        public List<SysUser> SelectNguoiDung(string taikhoan)
+        {
+            var c = from dt in dtcontent.SysUsers
+                    where dt.TaiKhoan == taikhoan
+                    select dt;
+            return c.ToList<SysUser>();
+        }
+        public List<SysUser> SelectNguoiDung_MaTKhau(string mk)
+        {
+            var c = from dt in dtcontent.SysUsers
+                    where dt.TaiKhoan == mk
+                    select dt;
+            return c.ToList<SysUser>();
+        }
+        public List<SysUser> GetNguoiDung_MaNhom(string manhom)
+        {
+            var c = from dt in dtcontent.SysUsers
+                    where dt.MaNhom == manhom
+                    select dt;
+            return c.ToList<SysUser>();
+        }
+        public bool insertNguoiDung(SysUser user)
+        {
+            try
+            {
+                SysUser nguoidung = new SysUser()
+                {
+                    ID = user.ID == null ? string.Empty : user.ID,
+                    TaiKhoan = user.TaiKhoan == null ? string.Empty : user.TaiKhoan,
+                    MatKhau = user.MatKhau == null ? string.Empty : user.MatKhau,
+                    HoTen = user.HoTen == null ? string.Empty : user.HoTen,
+                    MaNhom = user.MaNhom == null ? string.Empty : user.MaNhom,
+                    Email = user.Email == null ? string.Empty : user.Email,
+                    DienGiai = user.DienGiai == null ? string.Empty : user.DienGiai,
+                    HoatDong = user.HoatDong,
+                };
+                dtcontent.SysUsers.InsertOnSubmit(nguoidung);
+                dtcontent.SubmitChanges();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public bool UpdateNguoiDung(SysUser user)
+        {
+            try
+            {
+                SysUser nguoidung = dtcontent.SysUsers.Single(p => p.ID == user.ID);
+
+                nguoidung.TaiKhoan = user.TaiKhoan;
+                nguoidung.MatKhau = user.MatKhau == null ? string.Empty : user.MatKhau;
+                nguoidung.HoTen = user.HoTen == null ? string.Empty : user.HoTen;
+                nguoidung.MaNhom = user.MaNhom == null ? string.Empty : user.MaNhom;
+                nguoidung.Email = user.Email == null ? string.Empty : user.Email;
+                nguoidung.DienGiai = user.DienGiai == null ? string.Empty : user.DienGiai;
+                nguoidung.HoatDong = user.HoatDong;
+
+                dtcontent.SubmitChanges();
+
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+        public bool DeleteNguoiDung(string manguoidung)
+        {
+            try
+            {
+                var query = (from nguoidung in dtcontent.SysUsers
+                             where nguoidung.ID == manguoidung
+                             select nguoidung);
+                dtcontent.SysUsers.DeleteAllOnSubmit(query);
+                dtcontent.SubmitChanges();
+
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+    }
+}
