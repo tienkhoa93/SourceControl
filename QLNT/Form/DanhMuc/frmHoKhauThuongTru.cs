4b647681c8e3fa4a161fed564e690400ca3d190b
tienkhoa93 <1642030@student.hcmus.edu.vn>
2017-05-15 22:08:19 +0700

    - Design UI for form HoKhauThuongTru

---
+++ QLNT/Form/DanhMuc/frmHoKhauThuongTru.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DevExpress.XtraEditors;
+
+namespace QLNT.Form.DanhMuc
+{
+    public partial class frmHoKhauThuongTru : DevExpress.XtraEditors.XtraForm
+    {
+        public frmHoKhauThuongTru()
+        {
+            InitializeComponent();
+        }
+        private void btnHuyBo_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+        private void btnLuuThem_Click(object sender, EventArgs e)
+        {
+          
+        }
+        private void btnLuuDong_Click(object sender, EventArgs e)
+        { }
+        private void txtTenHoKhau_TextChanged(object sender, EventArgs e)
+        {
+        }
+    }
+     
+}
