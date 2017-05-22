b2cf4c56acebf19790f6d161c5389287b8dce6ad
ANONYMOUS\binhboong1001 <vuontoidinhcao6789@gmail.com>
2017-05-22 23:26:27 +0700

    no message

--- QLNT/Business/SysChucNang_Bus.cs
+++ QLNT/Business/SysChucNang_Bus.cs
@@ -10,6 +10,7 @@ namespace QLNT.Business
    public  class SysChucNang_Bus
     {
        QLNTDataContext dtcontent = new QLNTDataContext();
+
         public List<SysChucNang> SelectSysChucNang_Cap(string capcha, int cap)
         {
             var dt = from cn in dtcontent.SysChucNangs
